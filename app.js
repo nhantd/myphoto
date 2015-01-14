1275412a4b1e8a3f538d18ba70a63f64b8b046db
nhantd <nhantd@kaist.ac.kr>
2015-01-14 07:52:32 -0800

    fixed nodejs ejs

---
+++ app.js
@@ -0,0 +1,140 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var fs = require('fs');
+
+//var routes = require('./routes');
+function getIPAddresses() {
+
+    var ipAddresses = [];
+
+    var interfaces = require('os').networkInterfaces();
+    for (var devName in interfaces) {
+        var iface = interfaces[devName];
+        for (var i = 0; i < iface.length; i++) {
+            var alias = iface[i];
+            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+                ipAddresses.push(alias.address);
+            }
+        }
+    }
+
+    return ipAddresses;
+}
+ip=getIPAddresses();
+console.log(ip);
+
+var app = express();
+
+function processPost(request, response) {
+	var result = '';
+
+	request.on('data', function(data) {
+		console.log(data);
+	});
+	return result;	
+}
+
+
+function processGet() {
+    var result = '';
+
+    files = fs.readdirSync("./public/data");
+    //console.log(files);
+    for(var i = 0; i <files.length; i++) {
+    	result = result + "'"+files[i]+ "',";
+
+    }
+    return result;
+}
+
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', __dirname + '/views');
+app.use(express.bodyParser());
+app.set('view engine', 'ejs');
+
+/*app.get('/', function(req,res) {
+    res.sendfile('public/poc.html');
+});*/
+
+app.get('/', function(req,res){
+	//res.sendfile('public/poc.html');
+	files = fs.readdirSync('./public/data');
+	res.render('index.ejs',{files: files});
+});
+
+app.post('/upload',function(req,res){
+	file_data = req.body['file_data'].replace(/^data:image\/png;base64,/, '');
+	file_type = req.body['file_type'];
+	file_name = req.body['file_name'];
+	var outputFilename = path.join(__dirname, 'public')+'/data/'+file_name + '.png';
+	//console.log(file_data);
+
+	fs.writeFile(outputFilename, file_data,'base64', function(err) {
+		if(err) {
+			console.log(err);
+		} else {
+			console.log("JSON saved to " + outputFilename);
+		}
+	}); 
+
+/*	files = fs.readdirSync('./public/data');
+	res.render('index.ejs',{files: files});*/
+	/*res.writeHead(200, {'content-type': 'application/json'});
+	res.write(JSON.stringify({ success: true }));*/
+	//res.end();
+});
+
+
+app.post('/download',function(req,res){
+	console.log(req.body);
+	file_data = req.body['file_data_download'].replace(/^data:image\/png;base64,/, '');
+	//file_type = req.body['file_type'];
+	var outputFilename = path.join(__dirname, 'public')+'/download/save.' + 'png';
+	//console.log(file_data);
+
+	fs.writeFile(outputFilename, file_data,'base64', function(err) {
+		if(err) {
+			console.log(err);
+		} else {
+			console.log("JSON saved to " + outputFilename);
+		}
+	});
+	var file = fs.createReadStream(outputFilename);
+	res.writeHead(200, {"Content-Type" : "image/png"});
+	file.pipe(res); 
+
+/*	res.writeHead(200, {
+	    'Content-Type': 'application/octet-stream',
+	     //modification-date="date_object",
+	    'Content-Disposition': 'attachment',
+	     'filename' : 'public/upload/save.png' 
+	  });*/
+	//res.sendfile(outputFilename);
+});
+
+/*app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.set('view engine', 'ejs');
+*/
+
+
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
